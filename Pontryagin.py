8d8e9a24b1bf7447dc9e9f96fd0442360b6c5054
JoaquinMarquez <joaquinmarquezd@gmail.com>
2023-12-03 13:54:31 -0300

    Crea dinamica de x y p para Pontryagin

--- Pontryagin.py
+++ Pontryagin.py
@@ -42,32 +42,85 @@ def BangBangCustom(minimo: float, maximo: float):
         return valor
     return BangBang
 
-def crear_dinamica_x(A, B, a, epsilon):
+
+def crearDinamica_x(A,B,epsilon,a):
     """
-        Dada una matriz de interaccion A, una matriz de interaccion cruzada B\\
-        Y un parametro de inhibicion a, crea la funcion derivada de x.\\
+        Crea dinámica de u en funcion de una matriz de interaccion A\\
+        y una matriz de acoplamiento cruzado B, ademas de un factor de escala epsilon\\
+        ####################################################\n
+        Inputs:\\
+        A: Matriz de tamano nxn, es un numpy array\\
+        B: Matriz de tamano 2x2, es un numpy array\\
+        epsilon: float, es un factor de escala temporal\\
+        ####################################################\\
+        Outputs:\\
+        dinamica: Funcion que toma como entrada\\
+            x: Vector de estado de tamano 2n, con las primeras n siendo u,\\
+               las segundas n siendo v\\
+            C: Control, es un float\\
+            y tiene como salida f(x,C), que es un vector de tamano 2*N\\
     """
-    tamano = np.shape(A)[0]
-    def dinamica(t, x, C):
+    N = np.shape(A)[0]
+    def dinamica(x,C):
         """
-            Crea una dinamica para el vector x, que es un vector de tamaño 2*tamano\\
-            COMPLETAR ESTE DOCSTRING
+            Dinamica del vector de estado\\
         """
-        din = np.zeros(2*tamano)
-        for k in range(tamano):
-            din_u = x[k] - 1/3 * x[k] ** 3 - x[tamano+k]
-            inter_u = 0
-            for j in range(tamano):
-                inter_u += A[k,j] * (B[0,0] * (x[j] - x[k]) + B[0,1] * (x[tamano+j] - x[tamano+k]))
-            inter_u *= C
-            din_u += inter_u
-            din_u *= 1/epsilon
-            din[k] = din_u
-            din_v = x[k] + a
-            inter_v = 0
-            for j in range(tamano):
-                inter_v += A[k,j] * (B[1,0] * (x[j]- x[k]) + B[1,1] * (x[tamano+j]-x[tamano+k]))
-            din_v += C * inter_v
-            din[tamano+k] = din_v
-        return din
+        salida = np.zeros(2*N)
+        for k in range(N):
+            uk = x[k]
+            vk = x[N+k]
+            sin_sumatoria_u = (1/epsilon)*(uk - (1/3) * uk ** 3 - vk)
+            sin_sumatoria_v = uk + a
+            sumatoria_u = 0
+            sumatoria_v = 0
+            for j in range(N):
+                uj = x[j]
+                vj = x[N+j]
+                sumatoria_u += A[k,j] * (B[0,0] * (uj-uk) + B[0,1] * (vj-vk))
+                sumatoria_v += A[k,j] * (B[1,0] * (uj-uk) + B[1,1] * (vj-vk))
+            sumatoria_u *= C/epsilon
+            sumatoria_v *= C
+            sin_sumatoria_u += sumatoria_u
+            sin_sumatoria_v += sumatoria_v
+            salida[k] = sin_sumatoria_u
+            salida[N+k] = sin_sumatoria_v
+        return salida
+    return dinamica
+
+def crearDinamica_p(A, B, epsilon, a):
+    """
+        Crea función de dinamica de p
+    """
+    N = np.shape(A)[0]
+    def dinamica(x,p,C):
+        """
+            Dinamica de p
+        """
+        der_p = np.zeros(2*N)
+        for i in range(N):
+            ui = x[i]
+            pui = p[i]
+            pvi = p[N+i]
+            sum_nada = (1/epsilon) * pui * (1 - ui ** 2)
+            sum_control = 0
+            sum_u1 = 0
+            sum_u2 = 0
+            sum_v1 = 0
+            sum_v2 = 0
+            for j in range(N):
+                uj = x[j]
+                puj = p[j]
+                pvj = p[N+j]
+                sum_control += ui - uj
+                sum_u1 += puj * A[j,i]
+                sum_u2 += A[i,j]
+                sum_v1 += pvj * A[j,i]
+                sum_v2 += A[i,j]
+            final = 2 * sum_control + sum_nada + C*B[0,0]/epsilon * (sum_u1 - pui * sum_u2)\
+                    + pvi + C*B[1,0] * (sum_v1 - pvi * sum_v2)
+            der_p[i] = (-1) * final
+            final_v = - pui / epsilon + (C * B[0,1] / epsilon) * (sum_u1 - pui * sum_u2)\
+                      + C * B[1,1] * (sum_v1 - pvi * sum_v2)
+            der_p[N+i] = (-1) * final_v
+        return der_p
     return dinamica
